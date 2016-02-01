2e2de08fe6b1b2a5088c7e07c81745940e234c5e
Fridolin Pokorny <fpokorny@redhat.com>
2016-02-01 11:12:20 +0100

    Rename AbstractScenario to Scenario

--- scenarios/scenario.py
+++ scenarios/scenario.py
@@ -19,7 +19,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # ####################################################################
 
-class AbstractScenario(object):
+class Scenario(object):
 	def run(self, system):
 		raise NotImplemented
 
