28ff17ab7acd8e722bf6cbe53f466c292dd63a3e
Fridolin Pokorny <fpokorny@redhat.com>
2016-02-15 20:51:53 +0100

    Make ConnectionLocalCall{Sync,Async} inherited

--- common/system/connectionLocalCall.py
+++ common/system/connectionLocalCall.py
@@ -21,9 +21,10 @@
 
 import sys
 
-class ConnectionLocalCallAsync(object):
+class ConnectionLocallCall(object):
 	def __init__(self, system):
 		self._system = system
+		raise NotImplementedError()
 
 	def __getattr__(self, action):
 		service = self._system.get_service(action)
@@ -32,16 +33,14 @@ class ConnectionLocalCallAsync(object):
 		connection = self._system.get_connection(service['name'])
 		return connection.get_action(action, async = True)
 
-class ConnectionLocalCallSync(object):
+class ConnectionLocalCallAsync(ConnectionLocallCall):
+	# We can extend the functionality with local threads?
 	def __init__(self, system):
 		self._system = system
 
-	def __getattr__(self, action):
-		service = self._system.get_service(action)
-		if not self._system.is_storage(service['name']):
-			raise ValueError("Cannot run action from computational service on computational service")
-		connection = self._system.get_connection(service['name'])
-		return connection.get_action(action, async = False)
+class ConnectionLocalCallSync(ConnectionLocallCall):
+	def __init__(self, system):
+		self._system = system
 
 if __name__ == "__main__":
 	sys.exit(1)
