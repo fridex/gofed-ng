13b2da6d33904363d7e6939c194b1bf7ee11c09c
Fridolin Pokorny <fpokorny@redhat.com>
2016-02-17 17:41:40 +0100

    Introduce RpmService

---
+++ services/rpm/service.py
@@ -0,0 +1,42 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+# ####################################################################
+# gofed-ng - Golang system
+# Copyright (C) 2016  Fridolin Pokorny, fpokorny@redhat.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# ####################################################################
+
+import os
+from common.helpers.output import log
+from common.service.computationalService import ComputationalService
+from common.service.serviceEnvelope import ServiceEnvelope
+from common.service.action import action
+
+class RpmService(ComputationalService):
+	''' RPM and Source RPM related analysis '''
+
+	@action
+	def rpm_lint(self, file_id):
+		'''
+		Run rpmlint on given rpm
+		@param file_id: id of a file to run rpmlint on
+		@return: rpmlint output
+		'''
+		return "TODO"
+
+if __name__ == "__main__":
+	ServiceEnvelope.serve(RpmService)
+
