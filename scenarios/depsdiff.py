e6eb3bbce6e0ae012fa93a78d4573cbcd93583ba
Fridolin Pokorny <fpokorny@redhat.com>
2016-04-11 10:47:06 +0200

    scenario-DEPSDIFF: implementation

--- scenarios/depsdiff.py
+++ scenarios/depsdiff.py
@@ -20,6 +20,7 @@
 # ####################################################################
 
 import sys
+from plumbum import cli
 from common.helpers.utils import dict2json
 from scenario import Scenario
 
@@ -27,8 +28,66 @@ from scenario import Scenario
 class Depsdiff(Scenario):
     ''' get differences of dependencies '''
 
-    def main(self):
-        raise NotImplementedError()
+    language = cli.SwitchAttr("--language", str,
+                              help="specify project language",
+                              default="detect")
+
+    tool = cli.SwitchAttr("--tool", str,
+                          help="specify tool to analyse project with",
+                          default="default")
+
+    tool1 = cli.SwitchAttr("--tool1", str,
+                           help="specify tool to analyse project 1 with",
+                           default=None, excludes=["--tool"])
+
+    tool2 = cli.SwitchAttr("--tool2", str,
+                           help="specify tool to analyse project 2 with",
+                           default=None, excludes=["--tool"])
+
+    language1 = cli.SwitchAttr("--language1", str,
+                               help="specify language of project 1",
+                               default=None, excludes=["--language"])
+
+    language2 = cli.SwitchAttr("--language2", str,
+                               help="specify language of project 2",
+                               default=None, excludes=["--language"])
+
+    def construct_opts(self, first):
+        opts = {}
+        language = self.language1 if first else self.language2
+        tool = self.tool1 if first else self.tool2
+
+        if language:
+            opts['language'] = language
+        else:
+            opts['language'] = self.language
+
+        if tool:
+            opts['tool'] = tool
+        else:
+            opts['tool'] = self.tool
+
+        return opts
+
+    def main(self, project_file1, project_file2):
+        with self.get_system() as system:
+
+            opts1 = self.construct_opts(first=True)
+            opts2 = self.construct_opts(first=False)
+
+            with open(project_file1, 'r') as f:
+                file_id1 = system.async_call.upload(f.read())
+
+            with open(project_file2, 'r') as f:
+                file_id2 = system.async_call.upload(f.read())
+
+            deps1 = system.async_call.deps_analysis(file_id1.result, opts1)
+            deps2 = system.async_call.deps_analysis(file_id2.result, opts2)
+
+            depsdiff = system.async_call.deps_diff(deps1.result, deps2.result)
+
+            print dict2json(depsdiff.result)
+
         return 0
 
 if __name__ == '__main__':
