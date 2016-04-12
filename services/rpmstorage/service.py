f5bf89abf13cd2752635c35f09b1bc79d99c1d12
Fridolin Pokorny <fpokorny@redhat.com>
2016-04-12 12:54:32 +0200

    RPMSTORAGE: implementation

--- services/rpmstorage/service.py
+++ services/rpmstorage/service.py
@@ -19,13 +19,21 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # ####################################################################
 
+import os
+import re
+import urllib2
+from common.service.serviceResult import ServiceResult
 from common.helpers.output import log
-from common.helpers.utils import dict2json
+from common.service.dircache import Dircache
 from common.service.storageService import StorageService
 from common.service.serviceEnvelope import ServiceEnvelope
 from common.service.action import action
+from common.helpers.utils import remote_exists
+from common.system.fileId import FileId
+from common.helpers.file import blob_hash
 
 DEFAULT_RPM_DIR = 'rpms'
+DEFAULT_RPM_DIR_SIZE = '500M'
 
 
 class RpmStorageService(StorageService):
@@ -33,40 +41,112 @@ class RpmStorageService(StorageService):
 
     @classmethod
     def signal_startup(cls, config):
-        log.info("got startup signal")
-        log.info("custom config sections: " + dict2json(config))
         cls.rpm_dir = config.get('rpm-dir', DEFAULT_RPM_DIR)
+        cls.rpm_dir_size = config.get('rpm-dir-size', DEFAULT_RPM_DIR_SIZE)
+        cls.dircache = Dircache(cls.rpm_dir, cls.rpm_dir_size)
+        log.debug("Dircache size %sB " % cls.dircache.get_max_size())
+        log.debug("Dircache path '%s'" % cls.dircache.get_path())
 
-    @classmethod
-    def signal_termination(cls):
-        log.info("got termination signal")
+    @staticmethod
+    def _rpm_filename(name, version, release, distro, arch):
+        return "%s-%s-%s.%s.%s.rpm" % (name, version, release, distro, arch)
 
-    def signal_init(self):
-        log.info("got init signal")
-        self.rpm_dir = self.__class__.rpm_dir
+    def _rpm_url(self, name, version, release, distro, arch):
+        # example: https://kojipkgs.fedoraproject.org/packages/etcd/2.3.1/3.fc25/i686/etcd-2.3.1-3.fc25.i686.rpm
+        rpm_name =self._rpm_filename(name, version, release, distro, arch)
+        return "https://kojipkgs.fedoraproject.org/packages/%s/%s/%s.%s/%s/%s" \
+               % (name, version, release, distro, arch, rpm_name)
 
-    def signal_connect(self):
-        log.info("got connect signal")
+    @staticmethod
+    def _rpm_src_filename(name, version, release, distro):
+        return "%s-%s-%s.%s.src.rpm" % (name, version, release, distro)
 
-    def signal_disconnect(self):
-        log.info("got disconnect signal")
+    def _rpm_src_url(self, name, version, release, distro):
+        # example: https://kojipkgs.fedoraproject.org/packages/etcd/2.3.1/3.fc25/src/etcd-2.3.1-3.fc25.src.rpm
+        src_rpm_name = self._rpm_src_filename(name, version, release, distro)
+        return "https://kojipkgs.fedoraproject.org/packages/%s/%s/%s.%s/src/%s" \
+               % (name, version, release, distro, src_rpm_name)
 
-    def signal_process(self):
-        log.info("got process signal")
+    @staticmethod
+    def _rpm_parse_name(name):
+        print name
+        m = re.search("([a-z-_]+)-([a-z0-9_\.]+)-([a-z0-9_]+)\.([a-z0-9\.]+)\.(src|[a-z0-9_]+).rpm", name)
 
-    def signal_processed(self):
-        log.info("got processed signal")
+        if m is None:
+            raise ValueError("Unable to parse package name '%s'" % name)
+
+        ret = {
+            'name': m.group(1),
+            'version': m.group(2),
+            'release': m.group(3),
+            'distro': m.group(4),
+            'arch': None if m.group(5) == 'src' else m.group(5)
+        }
+
+        return ret
+
+    def _common_get(self, url, filename):
+        ret = ServiceResult()
+
+        with self.get_lock(filename):
+            if self.dircache.is_available(filename):
+                ret.result = FileId.construct(self, self.dircache.get_file_path(filename))
+            elif remote_exists(url):
+                log.debug("Downloading from %s" % (url,))
+                response = urllib2.urlopen(url)
+                blob = response.read()
+                h = blob_hash(blob)
+
+                self.dircache.store(blob, filename)
+
+                ret.result = FileId.construct(self, self.dircache.get_file_path(filename), hash_ = h)
+            else:
+                raise KeyError("Desired file '%s' does not exist ( %s )" % (filename, url))
+
+        ret.meta['origin'] = url
+        return ret
 
     @action
-    def rpm_get(self, package, arch=None, fedora_release=None):
+    def rpm_get_by_name(self, package):
+        '''
+        Get an RPM file id or source RPM file id by its fully qualified name (e.g. flannel-0.5.5-5.fc24.x86_64.rpm)
+        @param package:
+        @return:
+        '''
+        info = self._rpm_parse_name(package)
+        if info['arch']:
+            return self.rpm_get(info['name'], info['version'], info['release'], info['distro'], info['arch'])
+        else:
+            return self.rpm_src_get(info['name'], info['version'], info['release'], info['distro'])
+
+    @action
+    def rpm_get(self, package_name, version, release, distro, arch):
         '''
         Get an RPM file id
-        @param package: package name
-        @param arch: Fedora architecture identifier, if omitted "x86_64" is used
-        @param fedora_release: Fedora release, if omitted "rawhide" is used
-        @return: file id
+        @param package_name:
+        @param version:
+        @param release:
+        @param distro:
+        @param arch:
+        @return:
         '''
-        return "TODO"
+        url = self._rpm_url(package_name, version, release, distro, arch)
+        filename = self._rpm_filename(package_name, version, release, distro, arch)
+        return self._common_get(url, filename)
+
+    @action
+    def rpm_src_get(self, package_name, version, release, distro):
+        '''
+        Get a source RPM file id
+        @param package_name:
+        @param version:
+        @param release:
+        @param distro:
+        @return:
+        '''
+        url = self._rpm_src_url(package_name, version, release, distro)
+        filename = self._rpm_src_filename(package_name, version, release, distro)
+        return self._common_get(url, filename)
 
     @action
     def download(self, file_id):
@@ -75,7 +155,13 @@ class RpmStorageService(StorageService):
         @param file_id: id of the file that will be downloaded
         @return: file content
         '''
-        return "TODO"
+        filename = os.path.basename(file_id.get_identifier())
+
+        with self.get_lock(filename):
+            content = self.dircache.retrieve(filename)
+
+        return content
 
 if __name__ == "__main__":
     ServiceEnvelope.serve(RpmStorageService)
+
